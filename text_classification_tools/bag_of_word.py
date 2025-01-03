9594906d5fa82b528ff18c001050d409a4ec82b7
calebandria <kalebaandriamanaja@gmail.com>
2025-01-03 18:00:41 +0300

    adding rectification in some of debugging

--- text_classification_tools/bag_of_word.py
+++ text_classification_tools/bag_of_word.py
@@ -88,6 +88,9 @@ class BagOfWords:
     data_X, data_y = self.final_version_data(data)
     #train_review, test_review, train_label, test_label = train_test_split(data_X, data_y, test_size=0.2, random_state=42)
     cv_train_data = cv.fit_transform(data_X)
+    data_y = data_y.astype('int')
+
+    print("Number of NaN values in y after type conversion: ", {np.isnan(data_y).sum()})
     #cv_test_data = cv.transform(test_review)
 
     print("Train shape: ", cv_train_data.shape)
